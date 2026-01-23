897d412a9e6901ccdf6605e33f03a4d30a266cdd
Max Holmes <max.holmes@krystalclarity.com>
2026-01-23 14:20:42 +0000

    Hardcode variable

--- dbt_files/macros/generate_policy_aliases.sql
+++ dbt_files/macros/generate_policy_aliases.sql
@@ -1,19 +1,15 @@
 {% macro generate_policy_aliases(source_system) %}
     {#- 
-        Generates column aliases based on policy_mapping.yml file.
+        Generates column aliases based on policy_mapping var in dbt_project.yml.
         Args:
             source_system: 'circles' or 's_insurance'
     -#}
     
     {%- set source_column = source_system ~ '_column_name' -%}
-    
-    {%- set yaml_content -%}
-        {% include 'policy_mapping.yml' %}
-    {%- endset -%}
-    {%- set mapping_data = fromyaml(yaml_content) -%}
+    {%- set mapping_data = var('policy_mapping') -%}
     
     {%- set column_list = [] -%}
-    {%- for row in mapping_data['policy_mapping'] -%}
+    {%- for row in mapping_data -%}
         {%- set src_col = row[source_column] -%}
         {%- if src_col and src_col | trim != '' -%}
             {%- set _ = column_list.append(src_col ~ ' as ' ~ row['target_column_name']) -%}
