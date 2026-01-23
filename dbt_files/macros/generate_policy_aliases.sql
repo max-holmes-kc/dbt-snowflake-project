1d1a8deb88fc1ec0400d33bb7c984ee088025521
Max Holmes <max.holmes@krystalclarity.com>
2026-01-23 11:55:53 +0000

    Use yaml instead of csv

--- dbt_files/macros/generate_policy_aliases.sql
+++ dbt_files/macros/generate_policy_aliases.sql
@@ -1,28 +1,23 @@
 {% macro generate_policy_aliases(source_system) %}
     {#- 
-        Generates column aliases based on policy_mapping seed table.
+        Generates column aliases based on policy_mapping.yml file.
         Args:
             source_system: 'circles' or 's_insurance'
     -#}
     
     {%- set source_column = source_system ~ '_column_name' -%}
     
-    {%- set mapping_query -%}
-        select 
-            target_column_name,
-            {{ source_column }} as source_column_name
-        from {{ source('raw', 'policy_mapping') }}
-        where {{ source_column }} is not null 
-          and trim({{ source_column }}) != ''
+    {%- set yaml_content -%}
+        {% include 'policy_mapping.yml' %}
     {%- endset -%}
+    {%- set mapping_data = fromyaml(yaml_content) -%}
     
-    {%- set results = run_query(mapping_query) -%}
-    
-    {%- if execute -%}
-        {%- set column_list = [] -%}
-        {%- for row in results -%}
-            {%- set _ = column_list.append(row['SOURCE_COLUMN_NAME'] ~ ' as ' ~ row['TARGET_COLUMN_NAME']) -%}
-        {%- endfor -%}
-        {{ column_list | join(',\n        ') }}
-    {%- endif -%}
+    {%- set column_list = [] -%}
+    {%- for row in mapping_data['policy_mapping'] -%}
+        {%- set src_col = row[source_column] -%}
+        {%- if src_col and src_col | trim != '' -%}
+            {%- set _ = column_list.append(src_col ~ ' as ' ~ row['target_column_name']) -%}
+        {%- endif -%}
+    {%- endfor -%}
+    {{ column_list | join(',\n        ') }}
 {% endmacro %}
